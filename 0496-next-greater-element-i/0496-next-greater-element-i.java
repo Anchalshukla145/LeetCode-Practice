ee286b9d8c6124d520730a9f507d19845e8a1d5c
Anchal Shukla <152485349+Anchalshukla145@users.noreply.github.com>
2026-09-05 14:55:08 +0530

    Time: 3 ms (93.04%), Space: 45.6 MB (10.51%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,22 +1,17 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map<Integer,Integer> mp = new HashMap<>();
-        for(int i=0;i<nums1.length;i++){
-            int ele=nums1[i];
-            int count=-1;
-            for(int j=0;j<nums2.length;j++){
-                if(nums2[j]==ele){
-                    for(int k=j+1;k<nums2.length;k++){
-                if(ele<nums2[k]){
-                    count=nums2[k];
+    
+        for(int i=0;i<nums2.length;i++){
+            int ele=nums2[i];
+            int gt=-1;
+            for(int j=i+1;j<nums2.length;j++){
+                if(nums2[j]>ele){
+                    gt=nums2[j];
                     break;
                 }
             }
-            break;
-            }
-            }
-            
-                mp.put(ele,count);
+            mp.put(ele,gt);
         }
         int [] ans= new int[nums1.length];
         int j=0;
