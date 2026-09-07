74f2f289a7c6e0226d157e6e77475bdf224643f5
Anchal Shukla <152485349+Anchalshukla145@users.noreply.github.com>
2026-09-07 17:44:49 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,14 @@
 class Solution {
+    int [] memo= new int[100]; 
     public int climbStairs(int n) {
-        if (n <= 2) {
+        if (n == 2) {
             return n;
         }
+        if(n==1)return n;
+        if(memo[n]!=0)return memo[n];
+        memo[n]=climbStairs(n-1)+climbStairs(n-2);
+        return memo[n];
+
         
-        int prev2 = 1; 
-        int prev1 = 2; 
-        
-        for (int i = 3; i <= n; i++) {
-            int current = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = current;
-        }
-        
-        return prev1;
     }
 }
